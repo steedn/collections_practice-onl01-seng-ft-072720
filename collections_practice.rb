3128cda5f5a2435b272041623abf21b5df89488f
Nathan Steed <steedn@outlook.com>
2020-07-31 21:28:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,7 +24,8 @@ end
 def kesha_maker(array)
   new_array = []
   array.each do |word|
-    new_array << word.insert(2, "$")
+  word.insert(2, "$")
+  new_array << word[2] = ''
   end
   new_array
 end
